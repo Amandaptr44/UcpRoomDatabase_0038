035b91d18aca314df3a47426a561d24ae3d44e96
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-22 00:45:09 +0700

    Menambahkan jadwal baru ke database

---
+++ app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2pam.data.dao
+
+import androidx.compose.ui.input.pointer.PointerId
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2pam.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+
+interface JadwalDao {
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
