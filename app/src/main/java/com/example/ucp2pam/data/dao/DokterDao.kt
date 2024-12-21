3ced42cb898aa7c453ce12d3bd6a92fb122b9f15
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-22 00:42:26 +0700

    Menambahkan dokter baru ke database

--- app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
@@ -1,5 +1,6 @@
 package com.example.ucp2pam.data.dao
 
+import android.adservices.adid.AdId
 import androidx.room.Dao
 import androidx.room.Insert
 import com.example.ucp2pam.data.entity.Dokter
@@ -7,10 +8,7 @@ import kotlinx.coroutines.flow.Flow
 import androidx.room.Query as Query
 
 @Dao
-interface dokterDao {
+interface DokterDao {
     @Insert
     suspend fun insertDokter(dokter: Dokter)
 
-    @Query("SELECT * FROM dokter ORDER BY namaPasien ASC ")
-    fun getAllDokter() : Flow<List<Dokter>>
-}
