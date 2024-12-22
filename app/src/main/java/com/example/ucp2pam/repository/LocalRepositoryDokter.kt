3cc6230f8e155e02a7a4dc9d8b6b8f9213543cc7
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-22 23:55:24 +0700

    Menambahkan LocalRepositoryDokter agar mempermudah aplikasi untuk bekerja dengan data dokter

--- app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryDokter.kt
+++ app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryDokter.kt
@@ -1,4 +1,20 @@
 package com.example.ucp2pam.repository
 
-class LocalRepositoryDokter {
+import com.example.ucp2pam.data.dao.DokterDao
+import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+
+class LocalRepositoryDokter(private  val dokterDao: DokterDao) : RepositoryDokter{
+    override suspend fun insertDokter(dokter: Dokter) {
+        dokterDao.insertDokter(dokter)
+    }
+
+    override fun getAllDokter(): Flow<List<Dokter>> {
+        return dokterDao.getAllDokter()
+    }
+
+    override fun getDokter(dokterId: Long): Flow<Dokter> {
+        return dokterDao.getDokter(dokterId)
+    }
 }
