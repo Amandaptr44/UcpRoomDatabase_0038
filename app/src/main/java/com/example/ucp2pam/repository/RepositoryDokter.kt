06c08e4cfcc39a1034ad88f0bebebf9b6afd58b7
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-23 00:04:00 +0700

    Menambah RepositoryDokter untuk mengatur operasi terkait data dokter

--- app/src/main/java/com/example/ucp2pam/repository/RepositoryDokter.kt
+++ app/src/main/java/com/example/ucp2pam/repository/RepositoryDokter.kt
@@ -1,4 +1,12 @@
 package com.example.ucp2pam.repository
 
-interface RepositoryDokter {
+import android.adservices.adid.AdId
+import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDokter{
+    suspend fun insertDokter(dokter: Dokter)
+    fun getAllDokter(): Flow<List<Dokter>>
+    fun getDokter(dokterId: Long): Flow<Dokter>
 }
+
