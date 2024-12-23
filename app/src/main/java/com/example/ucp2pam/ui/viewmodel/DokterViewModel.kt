6331d5866c6fd2c9071cc7b063334191c9cc4019
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-23 21:08:05 +0700

    Tambahkan DokterEvent untuk menangani data input

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/DokterViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/DokterViewModel.kt
@@ -94,3 +94,12 @@ data class FormErrorStateDokter(
     }
 }
 
+data class DokterEvent(
+    val id: String = "",
+    val nama: String = "",
+    val spesialis: String = "",
+    val klinik: String = "",
+    val noHp: String = "",
+    val jamKerja: String = ""
+)
+
