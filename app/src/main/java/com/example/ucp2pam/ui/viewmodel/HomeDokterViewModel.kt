ea025d4ca961f87a6b34023a99ab9c7e10cdba61
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-24 07:15:14 +0700

    Tambahkan HomeDokterViewModel untuk mengelola status UI dengan repositoryDokter

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/HomeDokterViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2pam.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.entity.Dokter
+import com.example.ucp2pam.repository.RepositoryDokter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDokterViewModel(
+    private val repositoryDokter: RepositoryDokter
+) : ViewModel() {
