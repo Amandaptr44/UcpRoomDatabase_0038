08de4e9104d5b40cd3645d1cff6fe14f2ca9ce2f
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-24 14:06:09 +0700

    Tambahkan tujuan navigasi untuk layar HomeDokter, HomeJadwal, Detail, dan Update.

--- app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -1,2 +1,26 @@
 package com.example.ucp2pam.ui.navigation
 
+interface AlamatNavigasi{
+    val route: String
+}
+
+
+object DestinasiHomeDokter : AlamatNavigasi {
+    override val route = "homeDokter"
+}
+object DestinasiHomeJadwal : AlamatNavigasi {
+    override val route = "homeJadwal"
+}
+
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val jadwalId = "kodenya"
+    val routesWithArg = "$route/{$jadwalId}"
+}
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route = "update"
+    const val jadwalId = "kodenya"
+    val routesWithArg = "$route/{$jadwalId}"
+}
